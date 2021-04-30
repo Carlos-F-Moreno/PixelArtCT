874d67cf6da107093ec8d6af440140adf8644cb3
Fernando Moreno <fercano741@hotmail.com>
2021-04-29 22:29:58 -0300

    Parcheando la grilla infinita

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
+//Nos traemos los elementos necesarios para hacer un grillado exquisito//
 const columnas = document.getElementById("columnas");
 const filas =document.getElementById("filas");    
-
 const btnGenerarGrilla = document.getElementById("generarGrilla");
 const divContainer = document.getElementById("container");
 const colorElementoGrid = document.getElementById("inputColor");
 
 btnGenerarGrilla.addEventListener("click", function(){
-    
+//Esta linea borra la grilla ya generada//
+    divContainer.innerHTML = "";
+//Le decimos cuantas columnas debe tener//
     divContainer.style.gridTemplateColumns = `repeat(${columnas.value}, 1fr)`;
-
+//Creamos la grilla//
     for (let i = 0; i < columnas.valueAsNumber * filas.valueAsNumber; i++) {
+        //creamos los divs//
         const nuevoDiv = document.createElement("div");
+        //creamos la clase//
         nuevoDiv.classList.add("gridItem");
-        
+        //Evento de cambio de color//
         nuevoDiv.addEventListener("click", function(){
             nuevoDiv.style.backgroundColor = colorElementoGrid.value;
         })
+        //Agregamos los divs al contenedor//
         divContainer.appendChild(nuevoDiv);
     }
 });
