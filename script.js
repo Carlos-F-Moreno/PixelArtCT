ffef46e91087762919f5d84a77538692bcd2911a
Fernando Moreno <fercano741@hotmail.com>
2021-05-04 22:30:15 -0300

    Agregando Boton lapiz y boton goma

--- script.js
+++ script.js
@@ -1,30 +1,48 @@
 //Nos traemos los elementos necesarios para hacer un grillado exquisito//
 const columnas = document.getElementById("columnas");
-const filas =document.getElementById("filas");    
+const filas = document.getElementById("filas");
 const btnGenerarGrilla = document.getElementById("generarGrilla");
 const divContainer = document.getElementById("container");
-const colorElementoGrid = document.getElementById("inputColor");
+const colorElementoGrid = document.getElementById("inputColorLapiz");
+const btnMostarOcultarBordes = document.getElementById("mostarOcultarBordes");
+const btnLapiz = document.getElementById("btnLapiz");
+const btnGoma = document.getElementById("btnGoma");
+//variables//
+let modoLapiz = true;
+//
+btnLapiz.addEventListener("click", function () {
+    modoLapiz = true;
+});
+//
+btnGoma.addEventListener("click", function () {
+    modoLapiz = false;
+});
 
-btnGenerarGrilla.addEventListener("click", function(){
-//Esta linea borra la grilla ya generada//
+btnGenerarGrilla.addEventListener("click", function () {
+    //Esta linea borra la grilla ya generada//
     divContainer.innerHTML = "";
-//Le decimos cuantas columnas debe tener//
+    //Pintamos el fondo
+    divContainer.style.backgroundColor = inputColorFondo.value;
+    //Le decimos cuantas columnas debe tener//
     divContainer.style.gridTemplateColumns = `repeat(${columnas.value}, 1fr)`;
-//Creamos la grilla//
+    //Creamos la grilla//
     for (let i = 0; i < columnas.valueAsNumber * filas.valueAsNumber; i++) {
         //creamos los divs//
         const nuevoDiv = document.createElement("div");
         //creamos la clase//
         nuevoDiv.classList.add("gridItem");
         //Evento de cambio de color//
-        nuevoDiv.addEventListener("click", function(){
-            nuevoDiv.style.backgroundColor = colorElementoGrid.value;
-        })
-        //Evento para poner transparente los divs//
-        nuevoDiv.addEventListener("contextmenu", function(event){
-            event.preventDefault();
-            nuevoDiv.style.backgroundColor = "transparent";
-        })
+        nuevoDiv.addEventListener("click", function () {
+            if(modoLapiz === true){
+                nuevoDiv.style.backgroundColor = colorElementoGrid.value;
+            }else{
+                nuevoDiv.style.backgroundColor = "transparent";
+            }
+        });
+        //Para acultar los bordes//
+        btnMostarOcultarBordes.addEventListener("click", function () {
+            nuevoDiv.classList.toggle("bordesBlancos");
+        });
         //Agregamos los divs al contenedor//
         divContainer.appendChild(nuevoDiv);
     }
